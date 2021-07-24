10acabe1dd8867cbb844db35e6a85422c03a6297
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-07-24 16:34:34 -0500

    Added Spell Filters

--- api/spells.ts
+++ api/spells.ts
@@ -6,7 +6,16 @@ const cors = microCors();
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
   try {
-    const data = await fetchCollection("spells");
+    const spellList: any = request.query.spellList;
+
+    console.log(spellList);
+    const data = await fetchCollection(
+      "spells",
+      spellList
+        ? { $or: spellList.split(",").map((name: any) => ({ name })) }
+        : null
+    );
+
     response.status(200).send(data);
   } catch (e) {
     response.status(504).send(e);
