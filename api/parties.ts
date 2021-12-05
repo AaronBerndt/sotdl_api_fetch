624217b588e3a186cda242fad67e27d77550d6cd
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-12-05 16:19:44 -0600

    Added fetch by _id

--- api/parties.ts
+++ api/parties.ts
@@ -1,11 +1,17 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { fetchCollection } from "../utilities/MongoUtils";
 import microCors from "micro-cors";
+import { ObjectId } from "mongodb";
 const cors = microCors();
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
   try {
-    const data = await fetchCollection("parties");
+    const id: any = request.query._id;
+    const data = id
+      ? await fetchCollection("parties", {
+          _id: new ObjectId(id),
+        })
+      : await fetchCollection("parties");
     response.status(200).send(data);
   } catch (e) {
     response.status(504).send(e);
