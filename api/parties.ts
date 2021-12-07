67f7d0bd28eaef0439d83e49bcaad44168869434
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-12-07 07:16:11 -0600

    Added fetch party

--- api/parties.ts
+++ api/parties.ts
@@ -2,16 +2,37 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { fetchCollection } from "../utilities/MongoUtils";
 import microCors from "micro-cors";
 import { ObjectId } from "mongodb";
+import axios from "axios";
 const cors = microCors();
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
   try {
     const id: any = request.query._id;
-    const data = id
+    let data = id
       ? await fetchCollection("parties", {
           _id: new ObjectId(id),
         })
       : await fetchCollection("parties");
+
+    if (id) {
+      const { members } = data[0];
+      data = await Promise.all(
+        members.map(async (partyMemberId: string) => {
+          const url = `https://sotdl-api-fetch.vercel.app/api/characters?_id=${partyMemberId}`;
+
+          const { data: characterData } = await axios(url);
+          console.log(data);
+
+          return {
+            _id: partyMemberId,
+            name: characterData.name,
+            health: characterData.characteristics.Health,
+            damage: characterData.characterState.damage,
+          };
+        })
+      );
+    }
+
     response.status(200).send(data);
   } catch (e) {
     response.status(504).send(e);
