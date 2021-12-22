922d2ec50b57d09683e470b41d127e8970fa5bd6
Aaron <aberndt@nagios.com>
2021-12-22 08:07:53 -0600

    Changed to use ObjectId

--- api/monsters.ts
+++ api/monsters.ts
@@ -1,13 +1,14 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { fetchCollection } from "../utilities/MongoUtils";
 import microCors from "micro-cors";
+import {ObjectId} from "mongodb";
 const cors = microCors();
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
   try {
     const _id: any = request.query._id;
     const data = _id
-      ? await fetchCollection("monsters", { _id })
+      ? await fetchCollection("monsters", { _id: new ObjectId(_id) })
       : await fetchCollection("monsters");
 
     response.status(200).send(data);
