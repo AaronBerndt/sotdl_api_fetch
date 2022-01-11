4fb257579c73c1bbd49bd3b6932f3eb90247f939
AaronBerndt <aaron.berndt2@gmail.com>
2022-01-10 20:19:11 -0600

    Added mosnter

--- api/combats.ts
+++ api/combats.ts
@@ -19,7 +19,30 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
         `https://sotdl-api-fetch.vercel.app/api/parties?_id=${partyId}`
       );
 
-      data = { ...rest, partyId, combatants: [...combatants, ...partyData] };
+      const monsterData = await Promise.all(
+        combatants.map(async (combatant) => {
+          const { monsterId, damage, ...rest } = combatant;
+          let { data } = await axios(
+            `https://sotdl-api-fetch.vercel.app/api/monsters?_id=${combatant.monsterId}`
+          );
+          const maxHealth = data.characteristics.Health;
+          return {
+            ...rest,
+            damage,
+            monsterId,
+            currentHealth: maxHealth - damage <= 0 ? 0 : maxHealth - damage,
+            maxHealth,
+            ...data,
+          };
+        })
+      );
+
+      data = {
+        ...rest,
+        partyId,
+        combatants: monsterData,
+        ...partyData,
+      };
     }
 
     response.status(200).send(data);
