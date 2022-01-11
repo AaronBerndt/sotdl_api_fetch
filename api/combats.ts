fe561585dbbe6fa9878172fb9a8cb807640c8f9c
AaronBerndt <aaron.berndt2@gmail.com>
2022-01-10 20:10:57 -0600

    Added characterData to api

--- api/combats.ts
+++ api/combats.ts
@@ -2,16 +2,27 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { fetchCollection } from "../utilities/MongoUtils";
 import microCors from "micro-cors";
 import { ObjectId } from "mongodb";
+import axios from "axios";
 const cors = microCors();
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
   try {
     const id: any = request.query._id;
-    const data = id
+    let data = id
       ? await fetchCollection("combats", { _id: new ObjectId(id) })
       : await fetchCollection("combats", {});
 
-    response.status(200).send(id ? data[0] : data);
+    if (id) {
+      const { combatants, partyId, ...rest } = data[0];
+
+      const { data: partyData } = await axios(
+        `https://sotdl-api-fetch.vercel.app/api/parties?_id=${partyId}`
+      );
+
+      data = { ...rest, partyId, combatants: [...combatants, ...partyData] };
+    }
+
+    response.status(200).send(data);
   } catch (e) {
     response.status(504).send(e);
   }
