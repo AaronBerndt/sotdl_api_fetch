4fad759bfcbc05a30167b2c7788e01b80a3f7b7f
AaronBerndt <aaron.berndt2@gmail.com>
2022-05-15 13:42:09 -0500

    Added fetch effects api

---
+++ api/effects.ts
@@ -0,0 +1,20 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import microCors from "micro-cors";
+import { fetchCollection } from "../utilities/MongoUtils";
+import { orderBy } from "lodash";
+
+const cors = microCors();
+
+const handler = async (request: VercelRequest, response: VercelResponse) => {
+  try {
+    const name: any = request.query.name;
+    const data = name
+      ? await fetchCollection("effects", { name })
+      : await fetchCollection("effects");
+    response.status(200).send(orderBy(data, ["name"]));
+  } catch (e) {
+    response.status(504).send(e);
+  }
+};
+
+export default cors(handler);
